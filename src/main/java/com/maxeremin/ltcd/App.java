563ef6496b293d213d7077ed8e37f0882c251916
Maxim Eremin <eremin.max@gmail.com>
2020-02-03 17:20:30 -0800

    initial commit containing base files

---
+++ src/main/java/com/maxeremin/ltcd/App.java
@@ -0,0 +1,13 @@
+package com.maxeremin.ltcd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
